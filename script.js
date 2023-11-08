b1618e44e29c51103151511cf34502077c98a1ea
amar-polytechnic <150167979+amar-polytechnic@users.noreply.github.com>
2023-11-08 13:21:45 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const first_skill = document.querySelector(".skill:first-child");
+const sk_counters = document.querySelectorAll(".counter span");
+const progress_bars = document.querySelectorAll(".skills svg circle");
+
+window.addEventListener("scroll",()=>{
+    if(!skillsPlayed)
+    skillsCounter();
+})
+
+
+function hasReached(el){
+    let topPosition = el.getBoundingClientRect().top;
+    if(window.innerHeight >= topPosition + el.offsetHeight)return true;
+    return false;
+}
+
+function updateCount(num,maxNum){
+    let currentNum = +num.innerText;
+    
+    if(currentNum < maxNum){
+        num.innerText = currentNum + 1;
+        setTimeout(()=>{
+            updateCount(num,maxNum)
+        },12)
+    }
+}
+
+
+let skillsPlayed = false;
+
+function skillsCounter(){
+    if(!hasReached(first_skill))return;
+    skillsPlayed = true;
+    sk_counters.forEach((counter,i)=>{
+        let target = +counter.dataset.target;
+        let strokeValue = 735 - 250 * (target / 100);
+
+        progress_bars[i].style.setProperty("--target",strokeValue);
+
+        setTimeout(()=>{
+            updateCount(counter,target);
+        },400)
+    });
+
+    progress_bars.forEach(p => p.style.animation = "progress 2s ease-in-out forwards");
+}
